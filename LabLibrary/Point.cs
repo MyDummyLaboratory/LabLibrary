64b37b185c4289e388bb84c41616bf5a24a7c3ef
nrmontagne <nicolas.montagne@enpc.fr>
2022-03-29 15:45:50 +0200

    WIP: Comments to Point

--- LabLibrary/Point.cs
+++ LabLibrary/Point.cs
@@ -2,12 +2,24 @@
 
 namespace LabLibrary
 {
+    /// <summary>
+    /// Class defining a point in 3D euclidean space
+    /// </summary>
     public class Point
     {
+        /// <summary>
+        /// First coordinate of the point
+        /// </summary>
         public double X { get; set; }
         public double Y { get; set; }
         public double Z { get; set; }
 
+        /// <summary>
+        /// Initialises a new <see cref="Point"/> class.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
         public Point(double x, double y, double z)
         {
             X = x; Y = y; Z = z;
